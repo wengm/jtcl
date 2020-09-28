bd9e0664b047572cf402c252fe705ac40fa4fc73
wengm <crest_boy@yahoo.com>
2020-09-28 21:36:18 +0200

    Add pom.xml and add exception in logging

--- src/main/java/tcl/lang/cmd/ReadCmd.java
+++ src/main/java/tcl/lang/cmd/ReadCmd.java
@@ -32,112 +32,112 @@ import tcl.lang.channel.Channel;
 
 public class ReadCmd implements Command {
 
-	/**
-	 * This procedure is invoked to process the "read" Tcl command. See the user
-	 * documentation for details on what it does.
-	 * 
-	 * @param interp
-	 *            the current interpreter.
-	 * @param argv
-	 *            command arguments.
-	 */
+    /**
+     * This procedure is invoked to process the "read" Tcl command. See the user
+     * documentation for details on what it does.
+     * 
+     * @param interp
+     *            the current interpreter.
+     * @param argv
+     *            command arguments.
+     */
 
-	public void cmdProc(Interp interp, TclObject argv[]) throws TclException {
+    public void cmdProc(Interp interp, TclObject argv[]) throws TclException {
 
-		Channel chan; // The channel being operated on this
-		// method
-		int i = 1; // Index to the next arg in argv
-		int toRead = 0; // Number of bytes or chars to read from channel
-		boolean readAll = true; // If true read-all else toRead
-		boolean noNewline = false; // If true, strip the newline if there
-		TclObject result;
+        Channel chan; // The channel being operated on this
+        // method
+        int i = 1; // Index to the next arg in argv
+        int toRead = 0; // Number of bytes or chars to read from channel
+        boolean readAll = true; // If true read-all else toRead
+        boolean noNewline = false; // If true, strip the newline if there
+        TclObject result;
 
-		if ((argv.length != 2) && (argv.length != 3)) {
-			errorWrongNumArgs(interp, argv[0].toString());
-		}
+        if ((argv.length != 2) && (argv.length != 3)) {
+            errorWrongNumArgs(interp, argv[0].toString());
+        }
 
-		if (argv[i].toString().equals("-nonewline")) {
-			noNewline = true;
-			i++;
-		}
+        if (argv[i].toString().equals("-nonewline")) {
+            noNewline = true;
+            i++;
+        }
 
-		if (i == argv.length) {
-			errorWrongNumArgs(interp, argv[0].toString());
-		}
+        if (i == argv.length) {
+            errorWrongNumArgs(interp, argv[0].toString());
+        }
 
-		chan = TclIO.getChannel(interp, argv[i].toString());
-		if (chan == null) {
-			throw new TclException(interp, "can not find channel named \""
-					+ argv[i].toString() + "\"");
-		}
+        chan = TclIO.getChannel(interp, argv[i].toString());
+        if (chan == null) {
+            throw new TclException(interp, "can not find channel named \""
+                    + argv[i].toString() + "\"");
+        }
 
-		// Consumed channel name.
+        // Consumed channel name.
 
-		i++;
+        i++;
 
-		if (i==argv.length) {
-			readAll = true;
-		} else {
-			readAll = false;
-			if (Character.isDigit(argv[i].toString().charAt(0))) {
-				toRead = TclInteger.getInt(interp, argv[i]);
-			} else {
-				// this is a wierd error, but that's what io-32.3, iocmd-4.9 tests want
-				throw new TclException(interp, "bad argument \""+argv[i]+"\": should be \"nonewline\"");
-			}
-			
-		}
-	
-		try {
-			if (chan.getEncoding() == null && ! noNewline) {
-				result = TclByteArray.newInstance();
-			} else {
-				result = TclString.newInstance(new StringBuffer(64));
-			}
-			if (readAll) {
-				chan.read(interp, result, TclIO.READ_ALL, 0);
+        if (i==argv.length) {
+            readAll = true;
+        } else {
+            readAll = false;
+            if (Character.isDigit(argv[i].toString().charAt(0))) {
+                toRead = TclInteger.getInt(interp, argv[i]);
+            } else {
+                // this is a wierd error, but that's what io-32.3, iocmd-4.9 tests want
+                throw new TclException(interp, "bad argument \""+argv[i]+"\": should be \"nonewline\"");
+            }
+            
+        }
+    
+        try {
+            if (chan.getEncoding() == null && ! noNewline) {
+                result = TclByteArray.newInstance();
+            } else {
+                result = TclString.newInstance(new StringBuffer(64));
+            }
+            if (readAll) {
+                chan.read(interp, result, TclIO.READ_ALL, 0);
 
-				// If -nonewline was specified, and we have not hit EOF
-				// and the last char is a "\n", then remove it and return.
+                // If -nonewline was specified, and we have not hit EOF
+                // and the last char is a "\n", then remove it and return.
 
-				if (noNewline && chan.eof()) {
-					String inStr = result.toString();
-					if (inStr.endsWith("\n")) {
-						interp.setResult(inStr.substring(0, inStr.length()-1));
-					} else {
-						interp.setResult(result);
-					}
-				} else {
-					interp.setResult(result);
-				}
-			} else {
-				chan.read(interp, result, TclIO.READ_N_BYTES,
-						toRead);
-				interp.setResult(result);
-			}
+                if (noNewline && chan.eof()) {
+                    String inStr = result.toString();
+                    if (inStr.endsWith("\n")) {
+                        interp.setResult(inStr.substring(0, inStr.length()-1));
+                    } else {
+                        interp.setResult(result);
+                    }
+                } else {
+                    interp.setResult(result);
+                }
+            } else {
+                chan.read(interp, result, TclIO.READ_N_BYTES,
+                        toRead);
+                interp.setResult(result);
+            }
 
-		} catch (IOException e) {
-			throw new TclRuntimeError(
-					"ReadCmd.cmdProc() Error: IOException when reading "
-							+ chan.getChanName(), e);
-		}
-	}
+        } catch (IOException e) {
+            throw new TclRuntimeError(
+                    "ReadCmd.cmdProc() Error: IOException when reading "
+                            + chan.getChanName(), e);
+        }
+    }
 
-	/**
-	 * A unique error msg is printed for read, therefore dont call this instead
-	 * of the standard TclNumArgsException().
-	 * 
-	 * @param interp
-	 *            the current interpreter.
-	 * @param cmd
-	 *            the name of the command (extracted form argv[0] of cmdProc)
-	 */
+    /**
+     * A unique error msg is printed for read, therefore dont call this instead
+     * of the standard TclNumArgsException().
+     * 
+     * @param interp
+     *            the current interpreter.
+     * @param cmd
+     *            the name of the command (extracted form argv[0] of cmdProc)
+     */
 
-	private void errorWrongNumArgs(Interp interp, String cmd)
-			throws TclException {
-		throw new TclException(interp, "wrong # args: should be \""
-				+ "read channelId ?numChars?\" "
-				+ "or \"read ?-nonewline? channelId\"");
-	}
+    private void errorWrongNumArgs(Interp interp, String cmd)
+            throws TclException {
+        throw new TclException(interp, "wrong # args: should be \""
+                + "read channelId ?numChars?\" "
+                + "or \"read ?-nonewline? channelId\"");
+    }
 
 }
